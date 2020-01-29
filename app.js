48098cbe9be205aeb2e80988bff027615ae4e333
shelia <woman5650@gmainl.com>
2020-01-28 20:16:27 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,124 @@
+/*
+Loops
+
+for
+
+for 
+for in
+
+do while
+while
+
+foreach
+    
+break
+continue
+
+*/
+// console.log(arr[0]);
+
+let arr = ['Bryce', 'Bruce',  'Brian']
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index]; // arr[2]
+console.log(element); // Bryce
+}
+
+for (let i = 0; i < 10; i++){
+
+}
+ for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+}
+
+
+let obj = {
+    name: 'Bryce',
+    age: 42,
+    address: '123 Sesame Street'
+}
+for (const key in obj){
+    const element = obj[key]; //obj['name']
+    console.log(element); // do stuff
+
+}
+// let arr = ['Bryce', 'Bruce', 'Bryan']
+for (const idx in string) {
+    const element = string[idx];
+    console.log(element);
+do }
+     // stuff
+     console.log('Do while');
+
+} while (false);
+let count = 0;
+while (count > 1);
+
+while ( count< 2){
+    count++;
+    // do stuff
+    console.log('While');
+
+
+}
+while (false) {
+    // do stuff
+    console.log('While????');
+}
+
+
+//let names = ['Bryce', 'Bruce', 'Bryan', 'Bob']
+names.foreach(function (name,idx) {
+     console.log(name, idx);
+});
+
+for (const of names) {
+    if (name == 'Bruce') {
+        break;
+
+    }
+
+}
+
+
+}
+
+for (let i = 0; < data.length; i++) { // 0
+     Console.log(data{i});
+// here
+
+}
+console.log('loops been done');
+let bool = true;
+while (bool) {
+    // do stuff????
+
+    bool = false;
+
+}
+
+let newString = 'hello';
+let vowels = 0
+let cons = 0
+// loop through the string
+for (const letter of String) {
+    // look at each letter
+    let found = false;
+    for (let i = 0; i < vowels,.length; i++){
+    // if the letter is a vowel
+    if (letter == 'e'){
+    // add 1 to vowel counter
+    vowels++;[
+        //add 1 to vowel counter
+    
+        vowelCount++;
+        found = true;
+ }
+// log the results
+console.log(newString)
+console.log('vowels', vowels);
+console.log('cons',conCount);
+
+}
+
+        
+    }
